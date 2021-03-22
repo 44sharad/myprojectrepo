c2381a33c54a1cfb5d7b53b75804cf1a922835b1
44sharad <sharadp.3144@gmail.com>
2021-03-22 18:27:29 +0530

    added some code

--- SimpleGitDemo/src/main/Calculator.java
+++ SimpleGitDemo/src/main/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
 		System.out.println("Hii GIT HUB!!");
+		add(10,20);
 	}
 	public static void add(int num1,int num2){
 	      System.out.println(num1+num2);
