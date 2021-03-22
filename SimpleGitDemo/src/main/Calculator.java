75e14c1d67bffcb598f86fca4233cb05c928da96
44sharad <sharadp.3144@gmail.com>
2021-03-22 17:43:46 +0530

    second commit

--- SimpleGitDemo/src/main/Calculator.java
+++ SimpleGitDemo/src/main/Calculator.java
@@ -5,6 +5,12 @@ public class Calculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("Hii GIT HUB!!");
 	}
 
 }
+
+
+
+
+
