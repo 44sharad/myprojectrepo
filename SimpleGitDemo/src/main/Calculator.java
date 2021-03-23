45ebc5d37b4845fb06e0a8a08aab1c709572f4cd
44sharad <sharadp.3144@gmail.com>
2021-03-23 11:34:38 +0530

    func2 added

--- SimpleGitDemo/src/main/Calculator.java
+++ SimpleGitDemo/src/main/Calculator.java
@@ -39,7 +39,9 @@ public class Calculator {
 	public static void br1(int num1){
 	      System.out.println(num1* num1*num1);
 	  }
-	
+	public static void func2(int num1){
+	      System.out.println(num1* num1*num1);
+	  }
 }
 
 
