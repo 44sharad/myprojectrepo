b2f4da41d6ada8fafa30364994b6ed7eb8650693
44sharad <sharadp.3144@gmail.com>
2021-03-23 10:56:36 +0530

    master1 added function

--- SimpleGitDemo/src/main/Calculator.java
+++ SimpleGitDemo/src/main/Calculator.java
@@ -36,6 +36,9 @@ public class Calculator {
 	public static void cube(int num1){
 	      System.out.println(num1* num1*num1);
 	  }
+	public static void master1(int num1){
+	      System.out.println(num1* num1*num1);
+	  }
 }
 
 
